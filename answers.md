70426508ad3cd3f887901b94c6fb5e4632faf6ba
Alex040711 <zli44@tulane.edu>
2023-11-09 22:17:20 -0600

    Alex1109

--- answers.md
+++ answers.md
@@ -1,8 +1,63 @@
 # CMPS 2200 Assignment 4
+
 ## Answers
 
-**Name:**_________________________
-
+Name:  ________Alex Li________
 
 Place all written answers from `assignment-04.md` here for easier grading.
 
+
+Part 1
+
+
+a)
+
+1. Start with the amount N.
+2. Find the largest power of 2 that is less than or equal to N. Let this value be 2^k.
+3. Use one coin of denomination 2^k.
+4. Subtract 2^k from N.
+5. Repeat steps 2-4 until N becomes 0.
+
+
+
+b)
+
+Greedy-choice property: The greedy-choice property means making the best immediate choice at each step. In this case, it's always taking the highest denomination coin available. Since our denominations are powers of 2, you can never use fewer smaller coins than a single larger coin. For example, you can't use two 2^(k-1) coins instead of one 2^k coin because that would be the same amount but more coins.
+
+Optimal substructure property: The optimal substructure property means that an optimal solution to a problem contains optimal solutions to its subproblems. Here, after taking a coin of 2^k, you're left with N-2^k, which is a smaller version of the original problem. The greedy algorithm will also give the best solution for this remaining amount. Since this applies at every step, the final solution is optimal.
+
+In short, using the largest coin doesn't block us from finding the best solution for the rest of the money. Since this is true at every step, the greedy algorithm must give us the best overall solution.
+
+
+c)
+
+W(n)=O(log n)
+
+S(n)=O(log n)
+
+
+
+Part 2
+
+a) 
+
+The greedy algorithm may not always yield the fewest coins for arbitrary denominations. A counterexample is the set of denominations {1,3,4} for the amount 6. The greedy algorithm would choose two coins of 1 and one coin of 4, but the optimal solution is to use two coins of 3.
+
+
+b)
+
+Optimal substructure means that the optimal solution to a problem can be constructed from the optimal solutions of its subproblems. For the coin-changing problem, this means that if you have an optimal solution for N dollars, then for any amount less than N, the solution included for that smaller amount must also be optimal.
+
+If the solution for the smaller amount wasn't optimal, then replacing it with an optimal solution for the smaller amount would yield a better solution for N, contradicting our assumption that we had an optimal solution for N.
+
+c)
+
+1. Initialize an array dp of length N+1, representing the minimum number of coins needed for each amount from 0 to N. Set dp[0] to 0 since no coins are needed to make 0 dollars.
+2. For each amount i from 1 to N:
+   a. For each coin denomination D[ j ]:
+   * If D[ j ] is less than or equal to i, set dp[i] to the minimum of dp[i] and dp[ i -D[ j ]+1]
+3. The value at dp[N] gives the minimum number of coins needed to make `N` dollars.
+
+W=O(nk)
+
+S=O(n)
--- main.py
+++ main.py
@@ -19,13 +19,64 @@ def MED(S, T):
             return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
-def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+def fast_MED(S, T, memo=None):
+    # Initialize memo on the first call
+    if memo is None:
+        memo = {}
+    # If we have already solved this subproblem, return the answer.
+    if (len(S), len(T)) in memo:
+        return memo[(len(S), len(T))]
+    # Base cases
+    if S == "":
+        memo[(len(S), len(T))] = len(T)
+        return len(T)
+    elif T == "":
+        memo[(len(S), len(T))] = len(S)
+        return len(S)
+    # Recursive cases
+    if S[0] == T[0]:
+        # Characters are the same, move to the next character in both strings
+        memo[(len(S), len(T))] = fast_MED(S[1:], T[1:], memo)
+    else:
+        # Characters are different, consider all operations
+        insert_cost = fast_MED(S, T[1:], memo)
+        delete_cost = fast_MED(S[1:], T, memo)
+        replace_cost = fast_MED(S[1:], T[1:], memo)
+        memo[(len(S), len(T))] = 1 + min(insert_cost, delete_cost, replace_cost)
+    return memo[(len(S), len(T))]
 
-def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+def fast_align_MED(S, T, memo=None):
+    # Initialization of memo on the first call
+    if memo is None:
+        memo = {}
+    # Check if we have already solved this subproblem and have the alignment
+    if (len(S), len(T)) in memo:
+        return memo[(len(S), len(T))]
+    # Base cases with alignments
+    if S == "":
+        memo[(len(S), len(T))] = (len(T), T, "-"*len(T))
+        return len(T), T, "-"*len(T)
+    elif T == "":
+        memo[(len(S), len(T))] = (len(S), "-"*len(S), S)
+        return len(S), "-"*len(S), S
+    # Recursive cases
+    if S[0] == T[0]:
+        cost, alignS, alignT = fast_align_MED(S[1:], T[1:], memo)
+        memo[(len(S), len(T))] = (cost, S[0] + alignS, T[0] + alignT)
+        return cost, S[0] + alignS, T[0] + alignT
+    else:
+        insert_cost, insert_alignS, insert_alignT = fast_align_MED(S, T[1:], memo)
+        delete_cost, delete_alignS, delete_alignT = fast_align_MED(S[1:], T, memo)
+        replace_cost, replace_alignS, replace_alignT = fast_align_MED(S[1:], T[1:], memo)
+        # Choose the operation with the minimum cost
+        min_cost = min(insert_cost, delete_cost, replace_cost)
+        if min_cost == insert_cost:
+            memo[(len(S), len(T))] = (1 + insert_cost, "-" + insert_alignS, T[0] + insert_alignT)
+        elif min_cost == delete_cost:
+            memo[(len(S), len(T))] = (1 + delete_cost, S[0] + delete_alignS, "-" + delete_alignT)
+        else:
+            memo[(len(S), len(T))] = (1 + replace_cost, S[0] + replace_alignS, T[0] + replace_alignT)
+        return memo[(len(S), len(T))]
 
 def test_MED():
     for S, T in test_cases:
